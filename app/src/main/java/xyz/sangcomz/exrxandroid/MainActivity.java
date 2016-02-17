5442227f3f5a20118a6c6ef85852b3620d813986
sangcomz <sangcomz@naver.com>
2016-02-17 18:17:55 +0900

    test

--- app/src/main/java/xyz/sangcomz/exrxandroid/MainActivity.java
+++ app/src/main/java/xyz/sangcomz/exrxandroid/MainActivity.java
@@ -1,13 +1,53 @@
 package xyz.sangcomz.exrxandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import rx.Observer;
+import rx.Subscription;
+import rx.subjects.PublishSubject;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static PublishSubject<String> stringPublishSubject;
+    String a = "b";
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("a :::::: " + a);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        stringPublishSubject = PublishSubject.create();
+
+        Subscription subscription = stringPublishSubject.subscribe(new Observer<String>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("Observable completed");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("Oh no! Something wrong happened");
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println("s :::: " + s);
+                a = s;
+
+            }
+        });
+
+
+        startActivity(new Intent(this, Main2Activity.class));
+
     }
 }
